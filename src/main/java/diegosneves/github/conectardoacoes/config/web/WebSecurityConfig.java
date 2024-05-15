83c5874bc129c9e159ec8920d59a049443cadc89
Diego Neves <neves.diegoalex@outlook.com>
2024-05-15 15:38:14 -0300

    feat/i1 :wrench: Adiciona configurações de OpenAPI, segurança web e CORS
    Três classes de configuração foram criadas para aprimorar a documentação API, segurança web e compartilhamento de recursos de origem cruzada. A classe OpenApiConfig cuida da configuração da documentação da API, WebSecurityConfig define as configurações de segurança web e CorsConfig é responsável pela configuração de CORS.

---
+++ src/main/java/diegosneves/github/conectardoacoes/config/web/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package diegosneves.github.conectardoacoes.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * A classe {@link WebSecurityConfig} tem a responsabilidade de definir as configurações de segurança web desta aplicação.
+ *
+ * @author diegosneves
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /**
+     * Cria uma cadeia de filtros de segurança ({@link SecurityFilterChain}) para a instância {@link HttpSecurity} fornecida.
+     *
+     * @param http a instância de {@link HttpSecurity} que será configurada
+     * @return a cadeia de filtros de segurança criada ({@link SecurityFilterChain})
+     * @throws Exception se ocorrer um erro durante a configuração
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+}
