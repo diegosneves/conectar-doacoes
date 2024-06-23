caf02cfe13f195e5edbd20daa2c7a3d43bee9e18
Diego Neves <neves.diegoalex@outlook.com>
2024-06-22 21:55:11 -0300

    feat/i6 :white_check_mark: Adicione classes DonationMapper e DonationEntityMapper e seus respectivos testes
    Este commit introduz novas classes de mapeamento para gerenciar a conversão entre entidades de doação e objetos de domínio. Além disso, configura testes unitários correspondentes para garantir a funcionalidade correta dessas classes. Finalmente, a classe DonationFactory é incluída para lidar com a criação de instâncias de doação.

---
+++ src/test/java/diegosneves/github/conectardoacoes/adapters/rest/mapper/DonationEntityMapperTest.java
@@ -0,0 +1,53 @@
+package diegosneves.github.conectardoacoes.adapters.rest.mapper;
+
+import diegosneves.github.conectardoacoes.adapters.rest.exception.MapperFailureException;
+import diegosneves.github.conectardoacoes.adapters.rest.exception.ShelterEntityFailuresException;
+import diegosneves.github.conectardoacoes.adapters.rest.model.DonationEntity;
+import diegosneves.github.conectardoacoes.core.domain.shelter.entity.value.Donation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class DonationEntityMapperTest {
+
+    public static final String DONATION_ID = "28387d3e-e9cc-45b9-b87d-3ee9cc35b912";
+    public static final String DESCRIPTION = "Descricao";
+    public static final int AMOUNT = 1;
+
+
+    private DonationEntityMapper mapper;
+
+    private Donation donation;
+
+    @BeforeEach
+    void setUp() {
+        this.mapper = new DonationEntityMapper();
+        this.donation = new Donation(DONATION_ID, DESCRIPTION, AMOUNT);
+
+    }
+
+    @Test
+    void shouldMapToDonationEntity() {
+
+        DonationEntity donationEntity = this.mapper.mapFrom(this.donation);
+
+        assertNotNull(donationEntity);
+        assertEquals(DONATION_ID, donationEntity.getId());
+        assertEquals(DESCRIPTION, donationEntity.getDescription());
+        assertEquals(AMOUNT, donationEntity.getAmount());
+    }
+
+    @Test
+    void shouldThrowExceptionIfDonationIsNull() {
+
+        ShelterEntityFailuresException exception = assertThrows(ShelterEntityFailuresException.class, () -> this.mapper.mapFrom(null));
+
+        assertNotNull(exception);
+        assertEquals(ShelterEntityFailuresException.ERROR.formatErrorMessage(MapperFailureException.ERROR.formatErrorMessage(Donation.class.getSimpleName())), exception.getMessage());
+    }
+
+}
