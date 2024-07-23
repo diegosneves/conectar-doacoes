e2a7418514e639aa29b060f5055ed07c70faf500
Diego Neves <neves.diegoalex@outlook.com>
2024-07-22 23:09:09 -0300

    feat/i60 :art: Adiciona documentação detalhada e refatora o tratamento de exceções personalizadas
    Este commit adiciona comentários de documentação detalhada para a classe CustomException e seus construtores. Além disso, foi realizada uma reformulação do tratamento de exceções personalizadas no ControllerExceptionHandler, simplificando o processo para lidar apenas com a classe base CustomException, em vez de várias subclasses específicas. Desta forma, torna-se mais genérico e fácil de manter.

--- src/main/java/diegosneves/github/conectardoacoes/adapters/rest/exception/CustomException.java
+++ src/main/java/diegosneves/github/conectardoacoes/adapters/rest/exception/CustomException.java
@@ -21,26 +21,86 @@ public abstract class CustomException extends RuntimeException {
 
     private final ExceptionDetails errorDetails;
 
+    /**
+     * Este é um construtor protegido usado para criar uma nova instância de {@link CustomException} com um objeto {@link ExceptionDetails} especificado.
+     * <p>
+     * O objeto ExceptionDetails fornecido encapsula informações detalhadas de erro que são usadas para formatar a mensagem de erro
+     * que é passada para a classe {@link RuntimeException} pai. O objeto {@link ExceptionDetails} também é armazenado para recuperação posterior,
+     * permitindo que os detalhes sobre o erro estejam disponíveis ao tratar a exceção.
+     *
+     * @param error o objeto {@link ExceptionDetails} que contém as informações detalhadas de erro
+     */
     protected CustomException(ExceptionDetails error) {
         super(error.formatErrorMessage());
         this.errorDetails = error;
     }
 
+    /**
+     * Este é um construtor protegido usado para criar uma nova instância de {@link CustomException}.
+     * A mensagem de erro fornecida é formatada pelo objeto {@link ExceptionDetails} e passada para
+     * o construtor superclass {@link RuntimeException}.
+     * Além disso, o objeto {@link ExceptionDetails} é armazenado para recuperação posterior.
+     *
+     * @param message A mensagem de erro personalizada.
+     * @param error   O objeto {@link ExceptionDetails} que contém as informações detalhadas do erro.
+     */
     protected CustomException(String message, ExceptionDetails error) {
         super(error.formatErrorMessage(message));
         this.errorDetails = error;
     }
 
+    /**
+     * Este é um construtor protegido utilizado para criar uma nova instância de {@link CustomException}.
+     *
+     * <p>
+     * A instância é criada com uma causa {@link Throwable} especificada e um objeto {@link ExceptionDetails} também fornecido.
+     * A mensagem de erro formatada pelo objeto {@link ExceptionDetails} e a causa são passadas para o construtor da superclasse {@link RuntimeException}.
+     * O objeto {@link ExceptionDetails} é então armazenado neste objeto {@link CustomException} para recuperação futura.
+     *
+     * @param cause a causa (que é salva para recuperação posterior por {@link Throwable#getCause()}) (A null value is permitted,
+     *              and indicates that the cause is nonexistent or unknown).
+     * @param error o objeto {@link ExceptionDetails} que contém as informações mais detalhadas do erro.
+     */
     protected CustomException(Throwable cause, ExceptionDetails error) {
         super(error.formatErrorMessage(), cause);
         this.errorDetails = error;
     }
 
+    /**
+     * Construtor protegido para criar uma nova instância de {@link CustomException}.
+     * <p>
+     * Este construtor constrói o {@link CustomException} com uma mensagem personalizada de erro,
+     * uma causa (representada por um objeto {@link Throwable}) e um objeto detalhado da exceção
+     * (representado por um objeto {@link ExceptionDetails}).
+     * <p>
+     * A mensagem de erro é formatada pelo objeto {@link ExceptionDetails} e a causa são passadas
+     * para o construtor da superclasse {@link RuntimeException}. O objeto {@link ExceptionDetails}
+     * é armazenado dentro do objeto {@link CustomException} criado para a utilização futura.
+     *
+     * @param message A mensagem customizada de erro.
+     * @param cause   A causa do erro para fins de debug, representada por um objeto {@link Throwable}.
+     *                Um valor nulo é permitido, e indica que a causa é inexistente ou desconhecida.
+     * @param error   O objeto {@link ExceptionDetails} que contém as informações mais detalhadas do erro.
+     */
     protected CustomException(String message, Throwable cause, ExceptionDetails error) {
         super(error.formatErrorMessage(message), cause);
         this.errorDetails = error;
     }
 
+    /**
+     * Método getter para recuperar os detalhes do erro associados a esta {@link CustomException}.
+     *
+     * <p>
+     * Este método é usado para recuperar a instância de {@link ExceptionDetails} que foi fornecida
+     * ao construir esta {@link CustomException}. O objeto {@link ExceptionDetails} encapsula
+     * informações detalhadas sobre o erro que causou esta exceção, tais como a mensagem de erro e
+     * quaisquer outros dados relevantes relativos ao erro.
+     * </p>
+     *
+     * @return o objeto {@link ExceptionDetails} que encapsula as informações detalhadas sobre o
+     * erro que causou esta exceção. Isto pode ser nulo se nenhum detalhe foi fornecido quando a
+     * exceção foi construída.
+     */
     public ExceptionDetails getErrorDetails() {
         return this.errorDetails;
     }
